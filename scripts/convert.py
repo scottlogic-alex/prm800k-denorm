74b6d55a17e14654bc45bd7dd4e2c35026e1186f
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-07-12 14:37:00 +0100

    Add groundwork for conversation converter

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,1 +1,30 @@
-print('Hello world')
+import json
+from dataclasses import dataclass
+from typing import TypeAlias, Generator, Any
+
+Conversation: TypeAlias = Any
+
+
+@dataclass
+class Sample:
+    instruction: str
+    steps: str
+
+
+def make_telescoping_conversation(conversation: Conversation) -> Generator[Sample, None, None]:
+    instruction = conversation['question']['problem']
+    steps = []
+    for step in conversation['label']['steps']:
+        steps.append(step['completions'][step['chosen_completion']]['text'] if step['chosen_completion'] else step['human_completion']['text'])
+        yield Sample(
+            instruction=instruction,
+            steps=json.dumps(steps)
+        )
+
+
+def make_monologue(conversation: Conversation) -> Sample:
+    return Sample(
+        instruction=conversation['question']['problem'],
+        steps=json.dumps([step['completions'][step['chosen_completion']]['text'] if step['chosen_completion'] else step['human_completion']['text']
+                          for step in conversation['label']['steps']])
+    )
